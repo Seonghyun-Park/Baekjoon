6551911e10d444f179d49827090c0aee2087e62d
Seonghyun-Park <hyunni1206@gmail.com>
2023-02-13 16:57:35 +0900

    1000번: A+B

--- 1000.py
+++ 1000.py
@@ -1,2 +1,7 @@
-num = input().split(" ")
-print(int(num[0]) + int(num[1]))
+a, b = input().split()
+
+print(int(a) + int(b)) # input()함수는 기본적으로 문자형으로 취급하기 때문에 숫자는 형변환을 해주어야 한다.
+print(int(a) - int(b))
+print(int(a) * int(b))
+print(int(int(a) / int(b)))
+print(int(a) % int(b))
