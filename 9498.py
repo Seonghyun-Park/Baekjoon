848dbe65a342a4bb5af99f7b985fbb868f6d08cf
Seonghyun-Park <hyunni1206@gmail.com>
2023-02-14 16:52:12 +0900

    9498 : 시험성적

---
+++ 9498.py
@@ -0,0 +1,12 @@
+grade = int(input())
+
+if (90 <= grade):
+    print('A')
+elif(80 <= grade):
+    print('B')
+elif(70 <= grade):
+    print('C')
+elif(60 <= grade):
+    print('D')
+else:
+    print('F')
