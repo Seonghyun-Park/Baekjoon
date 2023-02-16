77c66900fcac4cbb923791d925fc634f5e597d6e
Park SeongHyun <121333241+Seonghyun-Park@users.noreply.github.com>
2023-02-16 23:38:33 +0900

    2480 : 주사위 세 개

--- 2480.py
+++ 2480.py
@@ -1,14 +1,14 @@
 # 주사위 세개
 a, b, c = map(int, input().split())
 
-if a == b == c:
+if a == b == c: # 세 수가 같을 때
     print(10000 + a * 1000)
-elif a == b or a == c or b == c:
+elif a == b or a == c or b == c: # 두 수가 같을 때
     if a == b:
         print(1000 + a * 100)
     elif a == c:
         print(1000 + c * 100)
     elif b == c:
         print(1000 + b * 100)
-else:
+else: # 세 수가 다를 때
     print(max(a, b, c) * 100) # max()는 최대값만 반환한다.
