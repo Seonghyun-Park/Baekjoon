1c67373e034dc5437e92002bf4b667940cca3f6a
Park SeongHyun <121333241+Seonghyun-Park@users.noreply.github.com>
2023-02-17 17:17:48 +0900

    Update 2884.py

--- 2884.py
+++ 2884.py
@@ -1,10 +1,10 @@
 # 알람시계
 h, m = map(int, input().split())
 
-if m >= 45:
+if m >= 45: # 45분이 지났을 때
     print(h, m - 45)
     
-elif m < 45:
+elif m < 45: # 45분이 지나지 않았을 때
     if h == 0: # 0시에서 23시로 돌아간다.
         print(h + 23, m + 15)
     else:
