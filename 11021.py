aeb09882ad53caed70eaae9e9236f15179b36d85
Seonghyun-Park <hyunni1206@gmail.com>
2023-02-19 16:53:23 +0900

    11021 : A+B - 7

---
+++ 11021.py
@@ -0,0 +1,13 @@
+# A+B - 7
+t = int(input())
+
+for i in range(t):
+    a, b = map(int, input().split())
+    print("Case #{}: {}".format(i + 1, a + b))
+    
+# A+B - 8
+t = int(input())
+
+for i in range(t):
+    a, b = map(int, input().split())
+    print("Case #{}: {} + {} = {}".format(i + 1, a, b, a+b))
