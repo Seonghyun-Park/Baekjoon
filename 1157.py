7d23b9a038fa6a261829c21905b41a241d85ae2e
Seonghyun-Park <hyunni1206@gmail.com>
2023-02-28 22:27:22 +0900

    1157 : 단어공부

--- 1157.py
+++ 1157.py
@@ -5,9 +5,9 @@ word_list = list(set(word)) # set을 사용하여 중복된 문자 제거
 cnt = []
 for i in word_list:
     count = word.count
-    cnt.append(count(i))
+    cnt.append(count(i)) # count 숫자를 리스트에 추가
     
-if cnt.count(max(cnt)) > 1:
+if cnt.count(max(cnt)) > 1: # count 숫자 최댓값이 중복되면
     print("?")
 else:
-    print(word_list[(cnt.index(max(cnt)))])
+    print(word_list[(cnt.index(max(cnt)))]) # 최댓값의 인덱스
