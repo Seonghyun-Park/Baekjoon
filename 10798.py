f4a534ad47a1c4b25f85b5ab68b1211638ebf59b
Seonghyun-Park <hyunni1206@gmail.com>
2023-03-19 22:39:16 +0900

    10798 : 세로 출력

--- 10798.py
+++ 10798.py
@@ -1,8 +1,15 @@
-import sys
-input = sys.stdin.readline
-
 word = []
+length = []
 
-for _ in range(5):
-    word.append(list(input().split()))
+for i in range(5):
+    word.append(input()) # 배열에 문자열을 담는다.
+    length.append(len(word[i])) # word가 배열이니까 배열의 각 인덱스(문자열)의 길이
     
+
+vertical_word = ''
+for col in range(max(length)): # 열의 최대 길이
+    for row in range(5): # 행의 길이 
+        if col < length[row]: # 열의 길이가 행의 길이보다 작으면
+            vertical_word += word[row][col] # 그냥 뛰어넘는다.
+
+print(vertical_word)
